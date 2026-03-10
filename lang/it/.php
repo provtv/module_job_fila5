15dacefffe469e23dd919081215ff3b11e995d2d
Nicola <storgatonicola@provincia.treviso.it>
2026-03-10 12:34:23 +0100

    .

--- lang/it/.php
+++ lang/it/.php
@@ -2,33 +2,27 @@
 
 declare(strict_types=1);
 
-return array (
-  'sections' => 
-  array (
-    'empty' => 
-    array (
-      'label' => 'empty',
-      'heading' => 'empty',
-    ),
-  ),
-  'actions' => 
-  array (
-    'logout' => 
-    array (
-      'tooltip' => 'logout',
-    ),
-  ),
-  'navigation' => 
-  array (
-    'label' => 'Missing Navigation Label',
-    'plural_label' => 'Missing Navigation Plural Label',
-    'group' => 'Missing Group',
-    'icon' => 'heroicon-o-puzzle-piece',
-    'sort' => 100,
-  ),
-  'label' => 'Missing Label',
-  'plural_label' => 'Missing Plural label',
-  'fields' => 
-  array (
-  ),
-);
+return [
+    'sections' => [
+        'empty' => [
+            'label' => 'empty',
+            'heading' => 'empty',
+        ],
+    ],
+    'actions' => [
+        'logout' => [
+            'tooltip' => 'logout',
+            'icon' => 'logout',
+        ],
+    ],
+    'navigation' => [
+        'label' => 'Missing Navigation Label',
+        'plural_label' => 'Missing Navigation Plural Label',
+        'group' => 'Missing Group',
+        'icon' => 'heroicon-o-puzzle-piece',
+        'sort' => 100,
+    ],
+    'label' => 'Missing Label',
+    'plural_label' => 'Missing Plural label',
+    'fields' => [],
+];
